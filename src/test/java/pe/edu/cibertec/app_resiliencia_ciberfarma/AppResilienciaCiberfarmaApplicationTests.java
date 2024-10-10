89a3172d4541ee9a95607c1f85de86f02493503d
AsumaToki04 <megamanzxu@hotmail.com>
2024-10-10 12:10:08 -0500

    Proyecto Creado

---
+++ src/test/java/pe/edu/cibertec/app_resiliencia_ciberfarma/AppResilienciaCiberfarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_resiliencia_ciberfarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppResilienciaCiberfarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
